38ab9e5053cbec31216a35bbb5ecd4f425675969
Valentin Valls <valentin.valls@esrf.fr>
2021-02-11 18:59:27 +0100

    Add an example

---
+++ atop_raw/utils.py
@@ -0,0 +1,37 @@
+class ProcessReport:
+    def __init__(self, reader, sstat):
+        pagesize = reader.header["pagesize"]
+
+        available_mem = sstat["mem"]["physmem"] * pagesize / 1024
+        available_mem = max(available_mem, 1)
+        self._available_mem = available_mem
+
+        ss_cpu = sstat["cpu"]
+        ss_cpu_all = sstat["cpu"]["all"]
+        available_ticks = (
+            ss_cpu_all["stime"]
+            + ss_cpu_all["utime"]
+            + ss_cpu_all["ntime"]
+            + ss_cpu_all["itime"]
+            + ss_cpu_all["wtime"]
+            + ss_cpu_all["Itime"]
+            + ss_cpu_all["Stime"]
+            + ss_cpu_all["steal"]
+            + ss_cpu_all["guest"]
+        )
+        available_ticks = available_ticks / ss_cpu["nrcpu"]
+        available_ticks = max(available_ticks, 1)
+        self._available_ticks = available_ticks
+
+    def get_cpu_percent(self, pstat):
+        """Compute the CPU load of a process as percent"""
+        stime = pstat["cpu"]["stime"]
+        utime = pstat["cpu"]["utime"]
+        cpu = (stime + utime) * 100 / self._available_ticks
+        return cpu
+
+    def get_mem_percent(self, pstat):
+        """Compute the memory usage of a process as percent"""
+        rmem = pstat["mem"]["rmem"]
+        mem = rmem * 100 / self._available_mem
+        return mem
---
+++ examples/read_file.py
@@ -0,0 +1,53 @@
+"""
+Example of use of the library.
+
+It parse a file read through all the content and display information on the fly.
+"""
+
+import sys
+import atop_raw
+from atop_raw.utils import ProcessReport
+
+
+if len(sys.argv) == 1:
+    raise RuntimeError("An atop file (at least) is expected")
+
+filenames = sys.argv[1:]
+for filename in filenames:
+    with open(filename, "rb") as stream:
+        print("=====================")
+        print(f"{filename}")
+        print("=====================")
+        reader = atop_raw.create_reader(stream)
+        print(reader.header)
+
+        for record in reader.records:
+            print()
+
+            # print(record.header)
+            # print(record.sstat)
+            sstat = record.sstat
+            report = ProcessReport(reader, sstat)
+
+            for pstat in record.pstats:
+                cmd = pstat["gen"]["cmdline"]
+                name = pstat["gen"]["name"]
+                pid = pstat["gen"]["pid"]
+                mem = report.get_mem_percent(pstat)
+                cpu = report.get_cpu_percent(pstat)
+                if mem < 1:
+                    continue
+                if cpu < 1:
+                    continue
+
+                if cmd == "":
+                    continue
+                if len(cmd) > 70:
+                    cmd = cmd[:70]
+                print(
+                    f"  {pid:>5} {name:<15} {cmd:<70}   mem:{mem: 5.1f}% cpu:{cpu: 5.1f}%"
+                )
+                # print(pstat)
+            print()
+            print(record.curtime)
+        print()
